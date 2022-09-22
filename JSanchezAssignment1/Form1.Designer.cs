dc2f259fdab3fb64e02b741d4c6360875d95c2db
Jaime <jaimicopanda@hotmail.com>
2022-09-22 15:19:39 -0400

    finished game

---
+++ JSanchezAssignment1/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace JSanchezAssignment1
+{
+    partial class frmTicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn00 = new System.Windows.Forms.Button();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn02 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn00
+            // 
+            this.btn00.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn00.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn00.Location = new System.Drawing.Point(26, 27);
+            this.btn00.Name = "btn00";
+            this.btn00.Size = new System.Drawing.Size(78, 79);
+            this.btn00.TabIndex = 0;
+            this.btn00.TabStop = false;
+            this.btn00.UseVisualStyleBackColor = true;
+            this.btn00.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn01
+            // 
+            this.btn01.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn01.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn01.Location = new System.Drawing.Point(131, 27);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(78, 79);
+            this.btn01.TabIndex = 1;
+            this.btn01.TabStop = false;
+            this.btn01.UseVisualStyleBackColor = true;
+            this.btn01.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn10
+            // 
+            this.btn10.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn10.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn10.Location = new System.Drawing.Point(26, 131);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(78, 79);
+            this.btn10.TabIndex = 2;
+            this.btn10.TabStop = false;
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn02
+            // 
+            this.btn02.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn02.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn02.Location = new System.Drawing.Point(231, 27);
+            this.btn02.Name = "btn02";
+            this.btn02.Size = new System.Drawing.Size(78, 79);
+            this.btn02.TabIndex = 1;
+            this.btn02.TabStop = false;
+            this.btn02.UseVisualStyleBackColor = true;
+            this.btn02.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn11
+            // 
+            this.btn11.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn11.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn11.Location = new System.Drawing.Point(131, 131);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(78, 79);
+            this.btn11.TabIndex = 2;
+            this.btn11.TabStop = false;
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn12.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn12.Location = new System.Drawing.Point(231, 131);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(78, 79);
+            this.btn12.TabIndex = 3;
+            this.btn12.TabStop = false;
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn22
+            // 
+            this.btn22.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn22.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn22.Location = new System.Drawing.Point(231, 234);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(78, 79);
+            this.btn22.TabIndex = 6;
+            this.btn22.TabStop = false;
+            this.btn22.UseVisualStyleBackColor = true;
+            this.btn22.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn21
+            // 
+            this.btn21.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn21.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn21.Location = new System.Drawing.Point(131, 234);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(78, 79);
+            this.btn21.TabIndex = 4;
+            this.btn21.TabStop = false;
+            this.btn21.UseVisualStyleBackColor = true;
+            this.btn21.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn20
+            // 
+            this.btn20.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn20.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn20.Location = new System.Drawing.Point(26, 234);
+            this.btn20.Name = "btn20";
+            this.btn20.Size = new System.Drawing.Size(78, 79);
+            this.btn20.TabIndex = 5;
+            this.btn20.TabStop = false;
+            this.btn20.UseVisualStyleBackColor = true;
+            this.btn20.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // frmTicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 348);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn20);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn02);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn01);
+            this.Controls.Add(this.btn00);
+            this.Name = "frmTicTacToe";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn00;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn02;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn20;
+    }
+}
+
---
+++ JSanchezAssignment1/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JSanchezAssignment1
+{
+    public partial class frmTicTacToe : Form
+    {
+        bool player = true;
+        int movements = 0;
+        public frmTicTacToe()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+
+            btn.Text = player ? "X" : "O";
+            btn.Enabled = false;
+            player = !player;
+            movements++;
+            this.CheckWinner(btn);
+
+        }
+
+
+        private void CheckWinner(Button btn)
+        {
+            bool winner = false;
+            string rowColumn = btn.Name.Replace("btn", "");
+            int currentRow = int.Parse(rowColumn[0].ToString());
+            int currentColumn = int.Parse(rowColumn[1].ToString());
+
+            if (CheckVertical(currentRow, currentColumn))
+            {
+                winner = true;
+            }
+            else if (CheckHorizontal(currentRow, currentColumn))
+            {
+                winner = true;
+            }
+            else if (CheckDiagonalRight(currentRow, currentColumn))
+            {
+                winner = true;
+            }
+            else if (CheckDiagonalLeft(currentRow, currentColumn))
+            {
+                winner = true;
+            }
+
+
+            if (winner)
+            {
+                DialogResult result = MessageBox.Show($"The {btn.Text} wins this match!!", "Winner");
+
+                if (result == DialogResult.OK)
+                {
+                    ResetGame();
+                }
+
+            }
+            else if (movements == 9 && !winner)
+            {
+                DialogResult result = MessageBox.Show($"This is a Tie match!!", "Tie");
+
+                if (result == DialogResult.OK)
+                {
+                    ResetGame();
+                }
+            }
+
+        }
+
+        private void ResetGame()
+        {
+            player = true;
+            movements = 0;
+            foreach (Button btn in this.Controls.OfType<Button>())
+            {
+                btn.Text = "";
+                btn.Enabled = true;
+            }
+        }
+
+        private bool CheckVertical(int row, int column)
+        {
+            int counter = 0;
+            string text = this.Controls.Find($"btn{row}{column}", true)[0].Text;
+            for (int i = 0; i < 3; i++)
+            {
+                if (this.Controls.Find($"btn{i}{column}", true)[0].Text == text)
+                {
+                    counter++;
+                }
+            }
+            return counter == 3;
+        }
+        private bool CheckHorizontal(int row, int column)
+        {
+            int counter = 0;
+            string text = this.Controls.Find($"btn{row}{column}", true)[0].Text;
+            for (int i = 0; i < 3; i++)
+            {
+                if (this.Controls.Find($"btn{row}{i}", true)[0].Text == text)
+                {
+                    counter++;
+                }
+            }
+            return counter == 3;
+        }
+
+        private bool CheckDiagonalRight(int row, int column)
+        {
+            int counter = 0;
+            string text = this.Controls.Find($"btn{row}{column}", true)[0].Text;
+            for (int i = 0; i < 3; i++)
+            {
+                if (this.Controls.Find($"btn{i}{i}", true)[0].Text == text)
+                {
+                    counter++;
+                }
+            }
+            return counter == 3;
+        }
+
+        private bool CheckDiagonalLeft(int row, int column)
+        {
+            int counter = 0;
+            string text = this.Controls.Find($"btn{row}{column}", true)[0].Text;
+            for (int i = 2, j = 0; i >= 0; i--, j++)
+            {
+                if (this.Controls.Find($"btn{i}{j}", true)[0].Text == text)
+                {
+                    counter++;
+                }
+            }
+            return counter == 3;
+        }
+
+
+    }
+}
